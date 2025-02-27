2b5af8072423369ecc1d037fb47c015855db4f0f
rrfernandop <ferponceulsa@gmail.com>
2025-02-26 20:59:12 -0600

    Tipos de datos
    Segundo código realizado en clase, identificar principales tipos de datos

---
+++ Unidad1/TipoDatos.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+    printf("%s","%d imprime datos decimales: 5.4\n");
+    printf("%s","%i imprime datos enteros: 2\n",2);
+    printf("%s","%o imprime datos octales: 6\n");
+    printf("%s","%x imprime datos hexadecimales: 8A1\n");
+    printf("%s","%u imprime datos enteros sin signo en decimal: 23\n");
+    printf("%s","%c imprime datos de caracteres: #\n");
+    printf("%s","%e imprime datos reales expresados en base y exponente:5^2 \n");
+    printf("%s","%f imprime datos reales escrito con punto decimal: 23.12\n");
+    printf("%s","%f imprime datos reales : 14\n");
+    printf("%s","%s imprime cadenas de caracteres: Hola123\n");
+    printf("%s","%lf imprime datos reales de tipo long double: 2.1646466565466642\n");
+
+ return 0;
+
+}
