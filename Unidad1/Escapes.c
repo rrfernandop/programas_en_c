0a8f1c73f8ccc31b71643aaaf6ed157916ca0378
rrfernandop <ferponceulsa@gmail.com>
2025-02-26 20:36:36 -0600

    Escapes
    Primer codigo realizado en clases

---
+++ Unidad1/Escapes.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main(){
+    printf("\\a Alarma\n");
+    printf("\\b Retroceso\n");
+    printf("\\f Avance de pagina\n");
+    printf("\\n Retorno de carro y avance de linea\n");
+    printf("\\r Fetorno de carro\n");
+    printf("\\t Tabulacion\n");
+    printf("\\\\ Diagonal invertida\n");
+    printf("\\? Signo de interrogacion\n");
+    printf("\\"" Comillas dobles\n");
+    printf("\\000 Octal\n");
+    printf("\\xhh Hexadecimal\n");
+    printf("\\0 Caracter nulo\n");
+
+
+return 0;
+
+}
